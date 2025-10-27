d60fdea6670daa2ce136a5824120d4588a349b55
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-10-27 17:43:14 -0400

    Add scaffold of color interface

--- source/main.jsx
+++ source/main.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-window.addEventListener("load", () => {
+import colorSchemes from "./colors.json"
+
+window.addEventListener("load", async () => {
   let root = createRoot(document.getElementById("root"));
-  root.render(<App />);
+  root.render(<App schemes={colorSchemes}/>);
 });
 
-function App() {
-  // Your app code goes here...
-  return <p>(This JSX code becomes the contents of your component)</p>;
+function App({ schemes }) {
+  let schemeComponents = [];
+
+  for (let scheme of schemes) {
+    schemeComponents.push(
+      <li key={scheme.id}>{scheme.title}</li>
+    );
+  }
+
+  return <ul>{schemeComponents}</ul>;
 }
