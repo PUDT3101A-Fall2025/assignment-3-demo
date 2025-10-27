6be6055cd09bc222ae5d3133a581816e318588bf
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-10-27 17:57:50 -0400

    Try changing CORS

--- source/main.jsx
+++ source/main.jsx
@@ -4,6 +4,13 @@ import { createRoot } from "react-dom/client";
 import colorSchemes from "./colors.json"
 
 window.addEventListener("load", async () => {
+  try {
+    let response = await fetch("https://www.colourlovers.com/api/palettes/top");
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+
   let root = createRoot(document.getElementById("root"));
   root.render(<App schemes={colorSchemes}/>);
 });
