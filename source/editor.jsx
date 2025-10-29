a549c7378ac648cbab88e3c8cfff5cb0ce7c75e6
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-10-29 17:31:42 -0400

    Set up interactivity

--- source/editor.jsx
+++ source/editor.jsx
@@ -1,43 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-
-import colorSchemes from "./colors.json"
 
 window.addEventListener("load", async () => {
   let root = createRoot(document.getElementById("root"));
   root.render(<Editor />);
 });
 
-let defaultColors = [
-  "#8865AA",
-  "#F5E67D",
-  "#F2B47A",
-  "#CC8E86",
-  "#6D3162"
-];
+let defaultColors = JSON.parse(localStorage.getItem("color-scheme"));
 
-function Editor({ schemes }) {
+if (!defaultColors) {
+  defaultColors = [
+    "#8865AA",
+    "#F5E67D",
+    "#F2B47A",
+    "#CC8E86",
+    "#6D3162"
+  ]
+}
+
+function Editor({ }) {
+  let [title, setTitle] = useState("My Color Scheme");
   let [colors, setColors] = useState(defaultColors);
 
   let swatches = [];
 
+  useEffect(() => {
+    localStorage.setItem("color-scheme", JSON.stringify(colors));
+  }, [colors]);
+
   for (let i = 0; i < colors.length; ++i) {
     let color = colors[i];
-    swatches.push(<Color color={color} key={color} />);
+    swatches.push(
+      <Swatch
+        color={color}
+        onDelete={() => setColors(colors.toSpliced(i, 1))}
+        onColorChange={(newColor) => setColors(colors.toSpliced(i, 1, newColor))}
+        canBeDeleted={colors.length > 1}
+      />
+    );
   }
 
   return <div id="editor">
+    <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}></input>
     <div className="scheme">
       {swatches}
-      <button className="new-swatch">+</button>
+      {(colors.length < 6) ? <button className="new-swatch" onClick={
+        () => setColors(colors.toSpliced(colors.length, 0, "#000000"))
+      }>+</button> : null}
     </div>
   </div>;
 }
 
-function Color({color}) {
+function Swatch({ color, onDelete, onColorChange, canBeDeleted }) {
   return <div className="swatch" style={{background: color}}>
-    <button className="delete">&#10006;</button>
+    {canBeDeleted ? 
+      <button className="delete" onClick={onDelete}>&#10006;</button> : null}
     <label>{color}</label>
-    <input className="picker" type="color" value={color}></input>
+    <input
+      className="picker"
+      type="color"
+      value={color}
+      onChange={(event) => onColorChange(event.target.value)}>  
+    </input>
   </div>
 }
